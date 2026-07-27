ea85a42c1f44ed8a8e78ca27a8ffc40caf0f0a15
Shreyanshu <8969tk@gmail.com>
2026-07-27 15:04:10 +0530

    5th

---
+++ Shreyanshu2024BTCS113/src/pages/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <h1 className="text-5xl text-center mt-20">
+      About Page
+    </h1>
+  );
+}
+
+export default About;
---
+++ Shreyanshu2024BTCS113/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <h1 className="text-5xl text-center mt-20">
+      Home Page
+    </h1>
+  );
+}
+
+export default Home;
